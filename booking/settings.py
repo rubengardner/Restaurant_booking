6052226e08284d7e671a6794a3a2bbcb0318a143
rubengardner <rgardnercuesta@gmail.com>
2023-02-14 16:32:41 +0000

    Add static files path to django settings

--- booking/settings.py
+++ booking/settings.py
@@ -25,6 +25,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'book_table/templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
