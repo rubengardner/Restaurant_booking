c4c57372d241cff9dec5f843baa921cb41fdfe25
rubengardner <rgardnercuesta@gmail.com>
2023-02-19 15:27:40 +0000

    Fix css links and media files

--- booking/settings.py
+++ booking/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
